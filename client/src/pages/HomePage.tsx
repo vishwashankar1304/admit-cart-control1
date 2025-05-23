1e0135ef4af481c30e8b65c7791a796120d33b32
Vishwa <vishwashankar1304@gmail.com>
2025-05-23 10:45:48 +0530

    change imaeg

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -77,7 +77,7 @@ const HomePage = () => {
           </div>
           <div className="hidden md:block">
             <img 
-              src='/lovable-uploads/images/products/siva.webp'
+              src='/lovable-uploads/images/products/siva.jpg'
               alt="Electricals Showcase"
               className="rounded-lg shadow-lg"
             />
