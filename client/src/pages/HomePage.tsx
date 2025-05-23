cdfb419e8a88f1d8631341b8689e04353ef92421
Vishwa <vishwashankar1304@gmail.com>
2025-05-23 10:53:51 +0530

    change imaeg

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -98,7 +98,7 @@ const HomePage = () => {
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {featuredProducts.map((product) => (
-              <ProductCard key={product._id} product={product} />
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         </div>
